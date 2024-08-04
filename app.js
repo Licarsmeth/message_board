b7777553578e705770adaed6072134a094158b17
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-08-04 10:01:21 +0545

    added messages array and they show up in views

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const indexRouter = require('./indexRouter');
 const newRouter = require('./newRouter');
 const app = express();
+const path = require("node:path");
+app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
 app.use("/new", newRouter);
