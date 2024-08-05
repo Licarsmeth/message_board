2653fb405fa33742274093acc865f871f0f775f8
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-08-05 09:24:48 +0545

    added express urlencoded for post

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const indexRouter = require('./indexRouter');
 const newRouter = require('./newRouter');
 const app = express();
 const path = require("node:path");
+
+//for ejs as view engine, looks for default path "views"
 app.set("view engine", "ejs");
+
+//for css and stuff
 const assetPath = path.join(__dirname, "public");
 app.use((express.static(assetPath)));
 
+//for the post methods to get url data in req.body
+app.use(express.urlencoded({extended: true}));
+
+//configure the various routes
 app.use("/", indexRouter);
 app.use("/new", newRouter);
 
--- newRouter.js
+++ newRouter.js
@@ -2,6 +2,5 @@ const { Router } = require("express");
 const newRouter = Router();
 
 newRouter.get("/", (req, res) => res.render("form"));
-// newRouter.post()
 
 module.exports = newRouter;
