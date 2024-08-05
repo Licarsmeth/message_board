1adedda049170a674cc3d452fbec59faa03e30a3
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-08-05 18:58:48 +0545

    added 0.0.0.0

--- app.js
+++ app.js
@@ -19,4 +19,4 @@ app.use("/", indexRouter);
 app.use("/new", newRouter);
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, "0.0.0.0", () => console.log(`Server running on port ${port}`));
